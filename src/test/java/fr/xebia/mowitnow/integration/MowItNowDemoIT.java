f7ba413e66d63dfe3add22631d25b1d2a790eaf3
snilyes <snoussi.ilyes@gmail.com>
2014-07-23 14:27:42 +0200

    ajouter un profile IT pour le test integration

--- src/test/java/fr/xebia/mowitnow/integration/MowItNowDemoIT.java
+++ src/test/java/fr/xebia/mowitnow/integration/MowItNowDemoIT.java
@@ -1,7 +1,5 @@
 package fr.xebia.mowitnow.integration;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,11 +11,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import fr.xebia.mowitnow.io.web.Application;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -27,18 +26,13 @@ public class MowItNowDemoIT {
 
   private WebDriver driver;
 
-  private ConfigurableApplicationContext context;
 
   private final String instructions = "5 5" + "\n1 2 N" + "\nGAGAGAGAA" + "\n3 3 E"
       + "\nAADAADADDA";
 
   @Before
   public void setUp() throws Exception {
-    // context = SpringApplication.run(Application.class, new String[] {});
-    // driver = new HtmlUnitDriver();
-    // ((HtmlUnitDriver) driver).setJavascriptEnabled(true);
-    driver = new FirefoxDriver();
-    driver.get(url());
+    driver = new FirefoxDriver(); 
   }
 
   private String url() {
@@ -48,11 +42,11 @@ public class MowItNowDemoIT {
   @After
   public void tearDown() throws Exception {
     driver.quit();
-    // SpringApplication.exit(context);
   }
 
   @Test
   public void demo() throws InterruptedException {
+	driver.get(url());
     driver.findElement(By.id("instructions")).clear();
     driver.findElement(By.id("instructions")).sendKeys(instructions);
     WebDriverWait wait = new WebDriverWait(driver, 20);
